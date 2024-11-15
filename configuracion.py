e7442b44bc21179de472c2fa21d64aaf8e0c4a4b
Vladimir Ortega <animatrix88@gmail.com>
2024-11-14 22:49:01 -0500

    Actualizacion archivo de configuracion entorno

--- configuracion.py
+++ configuracion.py
@@ -1,12 +1,31 @@
-class Configuracion:
-    # Configuración de Spark
-    JAR_SPARK = "C:\Spark\spark-3.5.3-bin-hadoop3\jars\mysql-connector-j-9.1.0"
-    NOMBRE_APP_SPARK = "AplicacionETL"
-    
-    # Configuración de conexión a MySQL
-    URL_JDBC = "jdbc:mysql://localhost:3307/filmdb"
-    PROPIEDADES_MYSQL = {
-        "user": "root",
-        "password": "admin",
-        "driver": "com.mysql.cj.jdbc.Driver"
-    }
+import logging
+import sys
+import pandas as pd
+import mysql.connector
+from sqlalchemy import create_engine
+from pyspark.sql import SparkSession
+import os
+
+
+# Clase de configuración
+class Config:
+    DB_HOST = "localhost"
+    DB_USER = "root"
+    DB_PASSWORD = "Admin123*"
+    DB_NAME = "films"
+    CSV_FILES = ['customer.csv', 'film.csv', 'inventory.csv', 'rental.csv', 'store.csv']
+
+# Clase de configuración de logging
+class LoggerSetup:
+    def __init__(self):
+        self.logger = logging.getLogger('FILM_ETL')
+        self.logger.setLevel(logging.INFO)
+        handler = logging.StreamHandler(sys.stdout)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+
+    def get_logger(self):
+        return self.logger
+    def get_logger(self):
+        return self.logger
