89296e8af7567b71d94a65c10d7b04afbaca5d35
Vladimir Ortega <animatrix88@gmail.com>
2024-11-14 22:48:30 -0500

    Actualizacion Archivo de Coneccion a Spark Base de Datos

--- coneccionSpark.py
+++ coneccionSpark.py
@@ -1,18 +1,30 @@
-from pyspark.sql import SparkSession
-from configuracion import Configuracion
+import mysql
 
-class ConectorSpark:
-    def __init__(self):
-        self.spark = SparkSession.builder \
-            .appName(Configuracion.NOMBRE_APP_SPARK) \
-            .config("spark.jars", Configuracion.JAR_SPARK) \
-            .getOrCreate()
-    
-    def leer_datos(self, consulta):
-        """Leer datos desde MySQL usando Spark"""
-        return self.spark.read.jdbc(url=Configuracion.URL_JDBC, table=f"({consulta}) as consulta_films", properties=Configuracion.PROPIEDADES_MYSQL)
-    
-    def detener(self):
-        """Detener la sesión Spark"""
-        self.spark.stop()
 
+class DatabaseConnector:
+    def __init__(self, config):
+        self.config = config
+
+    def create_mysql_connection(self, database=None):
+        return mysql.connector.connect(
+            host=self.config.DB_HOST,
+            user=self.config.DB_USER,
+            password=self.config.DB_PASSWORD,
+            database=database
+        )
+
+    def create_database_if_not_exists(self, logger):
+        try:
+            conn = self.create_mysql_connection()
+            cursor = conn.cursor()
+            cursor.execute(f"SHOW DATABASES LIKE '{self.config.DB_NAME}';")
+            if not cursor.fetchone():
+                cursor.execute(f"CREATE DATABASE {self.config.DB_NAME}")
+                conn.commit()
+                logger.info(f"Base de datos '{self.config.DB_NAME}' creada.")
+            else:
+                logger.info(f"La base de datos '{self.config.DB_NAME}' ya existe.")
+            cursor.close()
+            conn.close()
+        except mysql.connector.Error as e:
+            logger.error(f"Error al crear la base de datos: {e}")
