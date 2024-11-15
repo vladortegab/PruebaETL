4c285fba008afc427c220fe923076437f2658946
Vladimir Ortega <animatrix88@gmail.com>
2024-11-14 22:49:46 -0500

    Actualizacion archivo principal para ejecutar

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-from etl import ProcesoETL
+from configuracion import Config
+from configuracion import LoggerSetup 
+from coneccionSpark import DatabaseConnector 
+from etl import ETLProcess 
 
-def principal():
-    etl = ProcesoETL()
-    
-    # Definir consulta para el ETL
-    consulta = "SELECT * FROM film"
-    nombre_tabla = "film_organizado"
-    
-    # Ejecutar el proceso ETL
-    etl.ejecutar_etl(consulta, nombre_tabla)
+# Clase principal de la aplicación ETL
+class FilmsETLApp:
+    def __init__(self):
+        self.config = Config()
+        self.logger = LoggerSetup().get_logger()
+        self.db_connector = DatabaseConnector(self.config)
+        self.etl_process = ETLProcess(self.config, self.logger)
 
-if __name__ == "__main__":
-    principal()
+    def run(self):
+        self.logger.info("Iniciando proceso ETL del Proyecto Films")
+        self.db_connector.create_database_if_not_exists(self.logger)
+        self.etl_process.run_etl()
+        self.logger.info("Proceso ETL completado")
+
+if __name__ == '__main__':
+    app = FilmsETLApp()
+    app.run()
